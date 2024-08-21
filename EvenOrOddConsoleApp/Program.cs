c881f8ffd5b793248095963bc6b7d22190ac6b4d
Ckacio <ckacio@gmail.com>
2024-08-21 12:06:01 -0300

    Commit Inicial

---
+++ EvenOrOddConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+/* Define if a number is even or odd without using % */
+string EvenOrOdd(int number)
+{
+    return ((number - (2 * Convert.ToInt32(number / 2))) == 0 ? "Even" : "Odd");
+}
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"{i} is {EvenOrOdd(i)}");
+}
+
+//Agora Faça uma função que retorne TRUE, caso um determinado número for ímpar
+bool OddNumber(int number)
+{
+    return number % 2 != 0;
+}
+
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+List<int> oddNumbers = numbers.FindAll(OddNumber);
+
+foreach (int number in oddNumbers)
+{
+    Console.WriteLine(number);
+}
